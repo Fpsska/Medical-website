bd57c0c794bd43333b05022d69d29919d7ed5a9d
Fpsska <fpsska1337@gmail.com>
2022-11-23 13:03:02 +0300

    feat: create testimonials HTML section, add styles & install swiper npm package, initialize, configurate in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './index.html';
 import './assets/styles/style.css';
 
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
 // /. imports
 
-console.log('Hello!');
+// init Swiper:
+const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    loop: true
+});
