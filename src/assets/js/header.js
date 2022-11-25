af6df5d4142b0355b21d0db62c12f3cb65df2dc8
Fpsska <fpsska1337@gmail.com>
2022-11-25 21:39:39 +0300

    fix: incorrect logic of set fixed css-class for header HTML-el when its height is less/equal zero point

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -4,7 +4,7 @@ const header = document.querySelector('.header');
 
 window.addEventListener('scroll', () => {
     const { height } = header.getBoundingClientRect();
-    if (window.scrollY >= parseInt(height)) {
+    if (window.scrollY >= parseInt(height) && height > 0) {
         header.classList.add('fixed');
     } else {
         header.classList.remove('fixed');
