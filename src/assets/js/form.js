8b4510eea71111a21df36e31e0869b45cefa6006
Fpsska <fpsska1337@gmail.com>
2023-01-24 15:45:39 +0300

    feat: install imask npm package, create mask variable with IMask constructor with configuration , add condition for run all form logic in form.js file

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,17 +1,34 @@
+import IMask from 'imask';
+
+// /. imports
+
 const form = document.querySelector('.order__form');
 const nameInput = document.querySelector('.form__input--name');
 const phoneInput = document.querySelector('.form__input--phone');
 
 // /. variables
 
-phoneInput.addEventListener('input', function (e) {
-    const inputValue = e.target.value.replace(/[^0-9]/gi, '');
-    this.value = inputValue;
+// prohibit entering any characters other than numbers (native)
+// phoneInput.addEventListener('input', function (e) {
+//     const inputValue = e.target.value.replace(/[^0-9]/gi, '');
+//     this.value = inputValue;
+// });
+
+// or with usage imask plagin
+const mask = IMask(phoneInput, {
+    mask: '+{7} (000) 000-00-00',
+    lazy: false,
+    placeholderChar: '_'
 });
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     //
+
+    if (!mask.masked.isComplete) {
+        return;
+    }
+
     const formData = {
         userName: nameInput.value,
         userPhoneNum: phoneInput.value, // or + ${phoneInput.value}
